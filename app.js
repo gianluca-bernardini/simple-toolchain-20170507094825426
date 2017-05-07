db03c74b8f8b1ae8e519f7ac88f72876b57c8541
Gianluca Bernardini <gianluca.bernardini@it.ibm.com>
2017-05-07 14:03:39 +0000

    Revert "test commit"
    This reverts commit 71e14ac6c51e0c38eeb83ba3b7c141dbcb29396e.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
-console.log(appEnv.port);
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
 
